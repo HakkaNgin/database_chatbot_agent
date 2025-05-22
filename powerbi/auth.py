1283b386b5cf130a7e88acd2f9328e10e7fc0112
HakkaNgin <hurinturin123@gmail.com>
2025-05-22 19:25:13 +0800

    Change uvicorn[standard] to uvicorn in requirements.txt for debugging

--- powerbi/auth.py
+++ powerbi/auth.py
@@ -1,9 +1,9 @@
 from msal import ConfidentialClientApplication
 
 app = ConfidentialClientApplication(
-    client_id='YOUR_CLIENT_ID',
-    client_credential='YOUR_CLIENT_SECRET',
-    authority='https://login.microsoftonline.com/YOUR_TENANT_ID'
+    client_id='CLIENT_ID',
+    client_credential='CLIENT_SECRET',
+    authority='https://login.microsoftonline.com/TENANT_ID'
 )
 
 token_response = app.acquire_token_for_client(scopes=['https://analysis.windows.net/powerbi/api/.default'])
