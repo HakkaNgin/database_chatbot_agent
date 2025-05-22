1de36cb42eb7183815a3bd1d22ffd6ca6965b3f4
HakkaNgin <hurinturin123@gmail.com>
2025-05-22 12:14:40 +0800

    Initial structure of FastAPI

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+import openai
+import os
+
+app = FastAPI()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+@app.post("/chat")
+async def chat(request: Request):
+    data = await request.json()
+    user_query = data.get("query")
+    
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": user_query}]
+    )
+    return {"response": response["choices"][0]["message"]["content"]}
